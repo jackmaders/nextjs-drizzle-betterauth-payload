53a970d4a3b9371bef7422a38a6d4b2c93bf2dbc
Jack Maders <jackwmaders@gmail.com>
2025-11-10 11:13:07 +0000

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json', ".css"],},
-};
+const nextConfig: NextConfig = {};
 
 export default withPayload(nextConfig);
